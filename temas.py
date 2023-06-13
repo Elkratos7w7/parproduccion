915b6f197477a548646d2cc231d48c6067cf3834
Elkratos7w7 <128616525+Elkratos7w7@users.noreply.github.com>
2023-06-13 10:48:24 -0600

    Add files via upload

--- temas.py
+++ temas.py
@@ -1,2 +1,2 @@
-temas="Los temas que me cuestan entender son: pseudocodigo, formalizacionde datos, bucles."
+temas="Los temas que me cuestan entender son: pseudocodigo por que se me hace algo complicado al momento de formarlo, formalizacion de datos por que se me hace complicado enteder cada fase y ordenarlo, bucles por que ya ni me recuerdo que es."
 print(temas)
